b4a9212ad425f915205a21f2a5758dc54d8835a6
MTS13 <mtstaras@gmail.com>
2024-07-04 20:02:57 +0300

    make build

---
+++ index.a42e0d3c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2+"px";t.style.left=l,t.style.top=l,t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.a42e0d3c.js.map
